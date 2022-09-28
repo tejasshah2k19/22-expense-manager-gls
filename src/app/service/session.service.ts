ad9562163466adcbab88eb876d6e9b5b584576c5
tejas <tejasshah@gmail.com>
2022-09-28 18:50:56 +0530

    signup api

---
+++ src/app/service/session.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService {
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  signupApi(user: any) :Observable<any> {
+    console.log("SignupApi Call -> ");
+    console.log(user);
+   return this.httpClient.post("http://localhost:9999/register",user)
+  }
+
+}
